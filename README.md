179b3cddffae210623a17a6450615035644c9687
Joshua Spork <joshua.spork@storagecraft.com>
2017-04-20 16:50:09 -0600

    Adding SQL stuff

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# SwissPairingWebsite
-Swiss Pairing website largely intended for Warmachine/hordes EO's to make things easier
+# Tournament App
+Application largely intended for Warmachine/hordes EO's to make things easier
+
+I have three goals with this project:
+1. Create an open source community resource for running events/tournaments
+2. Create something easy to use
+3. Create something portable
+
+Goal 1:
+Its open sourced, license update to come later once I have something deliverable
+
+Goal 2:
+The ultimate form of this will be a website that is reachable by any browser at any time. Just sign in and view your active tournaments and pairings.
+
+Goal 3:
+Reporting will be done automatically from any participant and validated by the EO/TO and their delegates and will work on desktop and mobile browsers. No app, no fancy pluggins just log in and play
--- deploy/deploy.py
+++ deploy/deploy.py
@@ -18,25 +18,47 @@ EOF
 #updates installed packages
 subprocess.check_call("yum -y update",shell=True)
 #installs MariaDB
-subprocess.check_call("yum -y install MariaDB-server MariaDB-client", shell=True)
+subprocess.check_call("yum -y install MariaDB-server", shell=True)
 #Starts and enables the service for MariaDB
-subprocess.check_call("systemctl start mariadb", shell=True)
 subprocess.check_call("systemctl enable mariadb", shell=True)
+subprocess.check_call("systemctl start mariadb", shell=True)
 
 #connects to MariaDB
+engine = sqlalchemy.create_engine('mysql+pymysql://root:password@10.3.1.104', pool_recycle=3600)
 
 #Secures the database by running the commands in the mysql_secure_instalation script manually
 
 #creates the players database
+CREATE DATABASE `players` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 #creates the players.players table
-
+CREATE TABLE `players` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email_address` varchar(45) NOT NULL,
+  `phone_number` varchar(45) DEFAULT NULL,
+  `icon` blob,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `player_id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='player information table';
 #creates the players.history table
+#this should be a view based on the tournaments table
 
 #creates the models database
+CREATE DATABASE `models` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 #creates the models.models table
-
+CREATE TABLE `models` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `point_cost` int(11) NOT NULL,
+  `Faction` varchar(45) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  `image` blob,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 #creates the tournaments database
 
 #creates the tournaments.tournament sample table
